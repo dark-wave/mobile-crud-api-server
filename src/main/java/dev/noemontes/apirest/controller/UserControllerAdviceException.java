7e4819695190891fe4044c05c371d1e6512b77c6
dark-wave <noe.montes.developer@gmail.com>
2022-10-29 23:10:43 +0200

    comented code to resolve a bug

--- src/main/java/dev/noemontes/apirest/controller/UserControllerAdviceException.java
+++ src/main/java/dev/noemontes/apirest/controller/UserControllerAdviceException.java
@@ -1,15 +1,7 @@
 package dev.noemontes.apirest.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import dev.noemontes.apirest.exceptions.EmailDuplicatedException;
-
 @RestControllerAdvice
-public class UserControllerAdviceException extends ResponseEntityExceptionHandler{
-	
-	@ExceptionHandler(value = {EmailDuplicatedException.class})
-	public ResponseEntity<>
-}
+public class UserControllerAdviceException extends ResponseEntityExceptionHandler{}
